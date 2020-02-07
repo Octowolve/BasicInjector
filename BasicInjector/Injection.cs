ea0f24d6dd34f87107d09ea9797531e40ce3bc20
Aegis <32366759+z3r0Sec@users.noreply.github.com>
2020-02-07 18:55:00 +0100

    Projektdateien hinzufügen.

---
+++ BasicInjector/Injection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BasicInjector
+{
+    internal class Injection
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern int CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, IntPtr dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+
+        const int PROCESS_CREATE_THREAD = 0x0002;
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int PROCESS_VM_OPERATION = 0x0008;
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_READ = 0x0010;
+
+        // used for memory allocation
+        const uint MEM_COMMIT = 0x00001000;
+        const uint MEM_RESERVE = 0x00002000;
+        const uint PAGE_READWRITE = 4;
+
+        public bool InjectionPrep(string DLLLoc, int procID)
+        {
+            if (!File.Exists(DLLLoc))
+            {
+                System.Windows.MessageBox.Show("No DLL selected.", "ERROR");
+                return false;
+            }
+            return this.StartInjection(Convert.ToUInt32(procID), DLLLoc);
+        }
+
+        public bool StartInjection(uint pid, string dllToInject)
+        {
+            IntPtr intPtr = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, 0, pid);
+            if (intPtr == IntPtr.Zero)
+            {
+                return false;
+            }
+            IntPtr procAddress = Injection.GetProcAddress(Injection.GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+            if (procAddress == IntPtr.Zero)
+            {
+                return false;
+            }
+            IntPtr intPtr2 = Injection.VirtualAllocEx(intPtr, (IntPtr)null, (IntPtr)dllToInject.Length, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+            if (intPtr2 == IntPtr.Zero)
+            {
+                return false;
+            }
+            byte[] bytes = Encoding.ASCII.GetBytes(dllToInject);
+            if (Injection.WriteProcessMemory(intPtr, intPtr2, bytes, (uint)bytes.Length, 0) == 0)
+            {
+                return false;
+            }
+            if (Injection.CreateRemoteThread(intPtr, (IntPtr)null, IntPtr.Zero, procAddress, intPtr2, 0u, (IntPtr)null) == IntPtr.Zero)
+            {
+                return false;
+            }
+            Injection.CloseHandle(intPtr);
+            MainWindow._instance.AppendTextBox("Injection was successful");
+            return true;
+        }
+    }
+}
---
+++ BasicInjector/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BasicInjector
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow _instance;
+        private Injection inj = new Injection();
+        string dllloc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _instance = this;
+            GetRunningApps();
+        }
+
+        void GetRunningApps()
+        {
+            foreach (Process p in Process.GetProcesses("."))
+            {
+                try
+                {
+                    if (p.MainWindowTitle.Length > 0)
+                    {
+                        ComboboxItem combobox = new ComboboxItem();
+                        combobox.Text = p.MainWindowTitle;
+                        combobox.Value = p.Id;
+                        procListBox.Items.Add(combobox);
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void Inject_Click(object sender, RoutedEventArgs e)
+        {
+            if (procListBox.SelectedItem == null)
+            {
+                System.Windows.MessageBox.Show("No Selected Process", "ERROR");
+                return;
+            }
+            int id = Convert.ToInt32((procListBox.SelectedItem as ComboboxItem).Value);
+            if (!inj.InjectionPrep(dllloc, id))
+            {
+                System.Windows.MessageBox.Show("Couldnt inject the DLL!", "ERROR");
+                return;
+            }
+        }
+
+        private void OpenDllDirec_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            openFileDlg.Filter = "dll|*.DLL";
+            // Launch OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+            if (result == true)
+            {
+                dllloc = openFileDlg.FileName;
+                AppendTextBox("Loading DLL: " + dllloc);
+            }
+        }
+
+        public void AppendTextBox(string value)
+        {
+            logbox.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+            new Action(delegate ()
+            {
+                logbox.Text += value + Environment.NewLine;
+            }));
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (procListBox.Items.Count != 0)
+            {
+                AppendTextBox("Changed Process to: " + (procListBox.SelectedValue as ComboboxItem).Text + "\nProcess ID: " + (procListBox.SelectedValue as ComboboxItem).Value);
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            procListBox.Items.Clear();
+            GetRunningApps();
+        }
+    }
+
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
